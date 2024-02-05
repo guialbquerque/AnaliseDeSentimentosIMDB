404838b0c7d7ca391e456d9c32a4e221abfa47e0
Guilherme Bispo <guialbquerque@gmail.com>
2024-02-05 20:15:51 -0300

    function to plot the distribuition of classes was created

--- src/data_preparation/data_exploration.py
+++ src/data_preparation/data_exploration.py
@@ -1,7 +1,20 @@
 # Imports
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import warnings
 warnings.filterwarnings("ignore")
+
+
+def distribuition_plot(df, column):
+    colors = ['#ff9999', '#66b3ff']
+    fig = plt.figure(figsize=(6, 4))
+    sns.countplot(data = df, x = column, hue = column, palette = colors)
+    plt.xlabel('Sentimento')
+    plt.ylabel('Contagem')
+    plt.title('Avaliação do público', fontsize = 14, fontweight = 'bold')
+    plt.legend(labels = ['Negativo', 'Positivo'])
+    plt.show()
 
 
 def load_data(path):
