e58d8f0c5535badd209ae64851feed5c27bc01a0
Guilherme Bispo <guialbquerque@gmail.com>
2024-02-05 21:37:12 -0300

    function to understand the statistics of reviews was created, the length of avaliations was calculated and some statistics extracted

--- src/data_preparation/data_exploration.py
+++ src/data_preparation/data_exploration.py
@@ -6,17 +6,6 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def distribuition_plot(df, column):
-    colors = ['#ff9999', '#66b3ff']
-    fig = plt.figure(figsize=(6, 4))
-    sns.countplot(data = df, x = column, hue = column, palette = colors)
-    plt.xlabel('Sentimento')
-    plt.ylabel('Contagem')
-    plt.title('Avaliação do público', fontsize = 14, fontweight = 'bold')
-    plt.legend(labels = ['Negativo', 'Positivo'])
-    plt.show()
-
-
 def load_data(path):
     """function to load a dataset
 
@@ -32,3 +21,37 @@ def load_data(path):
     except FileNotFoundError:
         return None
     
+
+def distribuition_plot(df, column):
+    colors = ['#ff9999', '#66b3ff']
+    fig = plt.figure(figsize=(6, 4))
+    sns.countplot(data = df, x = column, hue = column, palette = colors)
+    plt.xlabel('Sentimento')
+    plt.ylabel('Contagem')
+    plt.title('Avaliação do público', fontsize = 14, fontweight = 'bold')
+    plt.legend(labels = ['Negativo', 'Positivo'])
+    plt.show()
+
+
+def reviews_statistic(df):
+    length_reviews = df['Reviews'].apply(lambda x: len(x))
+    Reviews_distribuition = length_reviews.describe()
+    Reviews_distribuition.index = ['Quantidade', 
+                                   'Média',
+                                   'Desvio Padrão',
+                                   'Mínimo',
+                                   '25%',
+                                   '50%', 
+                                   '75%',
+                                   'Máximo']
+    reviews = Reviews_distribuition.to_frame()
+    reviews.drop(['Quantidade',
+                  '25%',
+                  '50%',
+                  '75%'],
+                 axis=0,
+                 inplace=True)
+    reviews.rename(columns = {'Reviews': 'Valor'}, inplace=True)
+    reviews = reviews.apply(lambda x: round(x, 2))
+    reviews.index.name = 'Estatísticas'
+    return reviews
