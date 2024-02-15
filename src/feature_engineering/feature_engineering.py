84ddf6cc0a7db2390dccb4d7a6a9103d6d430877
Guilherme Bispo <guialbquerque@gmail.com>
2024-02-15 19:43:22 -0300

    Function to remove stop words was created, and function to visualize the most frequent words

--- src/feature_engineering/feature_engineering.py
+++ src/feature_engineering/feature_engineering.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+import nltk
+from nltk import tokenize
+import pandas as pd
+import seaborn as sns
+nltk.download('stopwords')
 
 def positive_words_cloud(text, column_text):
 
@@ -25,4 +30,35 @@ def negative_words_cloud(text, column_text):
     plt.savefig('/home/guilherme/AIEnvironment/SentimentAnalysis/reports/figures/negative_words_cloud.png')
     plt.show()
     
+
+def plot_word_frequencies(text, column_text, quantity):
     
+    all_words = ' '.join([text for text in text[column_text]])
+    token = tokenize.WhitespaceTokenizer().tokenize(all_words)
+    frequency = nltk.FreqDist(token)
+    df_frequency = pd.DataFrame({"Palavra": list(frequency.keys()), "Frequência": list(frequency.values())})
+    df_frequency = df_frequency.nlargest(n = quantity, columns = 'Frequência')
+    plt.figure(figsize = (8,6))
+    ax = sns.barplot(data = df_frequency, x = 'Palavra', y = 'Frequência')
+    ax.set_xlabel('Palavra', fontsize=14)
+    ax.set_ylabel('Frequência', fontsize=14)
+    ax.xaxis.label.set_color('black')
+    ax.yaxis.label.set_color('black')
+    ax.tick_params(axis = 'x', colors = 'black')
+    ax.tick_params(axis = 'y', colors = 'black')
+    plt.show()
+
+
+def remove_stop_words(df, df_column):
+    stop_words = nltk.corpus.stopwords.words('english')
+    new_texts = list()
+    for opinion in df[df_column]:
+        news = list()
+        word_texts = tokenize.WhitespaceTokenizer().tokenize(opinion)
+        for word in word_texts:
+            if word not in stop_words:
+                news.append(word)
+        new_texts.append(' '.join(news))
+    return new_texts
+        
+            
